efa34cb90b45c2e2f36b1af51c9879594dc49e4f
Dason Kurkiewicz <dasonk@iastate.edu>
2012-10-04 00:54:08 -0500

    Fixed bug that omitted argument names from documentation

--- R/argumentsCheck.R
+++ R/argumentsCheck.R
@@ -2,8 +2,13 @@
 # if they aren't proper then it sets some defaults
 argumentsCheck <- function(fun){
     #TODO: If Arguments is set check that it is proper
-    argnames <- names(formals(fun))
-    n <- length(argnames)
-    default <- paste(argnames, paste("argument", seq(n)))
-    nonNullDefault(Arguments(fun), default)
+    arg <- Arguments(fun)
+    if(!is.null(arg)){
+        paste(names(arg), arg)
+    }else{
+        argnames <- names(formals(fun))
+        n <- length(argnames)
+        default <- paste(argnames, paste("argument", seq(n)))
+        default
+    }
 }
