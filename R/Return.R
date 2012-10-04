f01328db2064775469c1cca4722cee1f4c3b7ab8
Dason Kurkiewicz <dasonk@iastate.edu>
2012-10-04 00:55:02 -0500

    Added some stuff for Return

--- R/Return.R
+++ R/Return.R
@@ -1,9 +1,11 @@
 #' Look at flydoc return
 #' 
 #' Look at the the return for your flydoc documnetation
-#' for a given function
+#' for a given function.  This is what drives the section
+#' labeled "Value" in the help files.
 #' 
 #' @param fun A function
+#' @aliases Value
 #' @export
 Return <- function(fun){
     fdGet("Return", fun)
@@ -12,7 +14,8 @@ Return <- function(fun){
 #' Set documentation for return
 #' 
 #' Set a return for your flydoc documentation for a
-#' given function
+#' given function. This is what drive the section
+#' labeled "Value" in the help files.
 #' 
 #' @param func A function to add a flydoc return to.
 #' @param value A character string containing the 
