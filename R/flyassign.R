8222766e33a62f67e79c41e0ca1bf168396f287a
Dason Kurkiewicz <dasonk@iastate.edu>
2012-10-07 16:51:53 -0500

    Added comments

--- R/flyassign.R
+++ R/flyassign.R
@@ -1,3 +1,5 @@
+# Internal function so that I can change how
+# the documentation is stored if I want.
 flyassign <- function(name, fun, value){
     fun <- flycheck(fun)
     assign(name, value, envir = attr(fun, "flydoc"))
--- R/flyget.R
+++ R/flyget.R
@@ -1,3 +1,5 @@
+# Internal function so that I can change how
+# the documentation is stored if I want.
 flyget <- function(value, fun){
         tryCatch(get(value, envir = attr(fun, "flydoc")), 
              error = function(e){NULL})
